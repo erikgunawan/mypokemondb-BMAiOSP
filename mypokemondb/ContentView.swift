915291e73a6bafb12858e278c323d492057c2d61
erikgunawan <erik.gunawan44@gmail.com>
2026-04-03 21:29:21 +0700

    Initial Commit

---
+++ mypokemondb/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mypokemondb
+//
+//  Created by Erik Gunawan on 03/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mypokemondb/mypokemondbApp.swift
@@ -0,0 +1,17 @@
+//
+//  mypokemondbApp.swift
+//  mypokemondb
+//
+//  Created by Erik Gunawan on 03/04/26.
+//
+
+import SwiftUI
+
+@main
+struct mypokemondbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
